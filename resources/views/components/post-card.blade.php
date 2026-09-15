fb9f048d8766cb26328776827d89328886e61a55
prayoga-hk <prayogahk26@gmail.com>
2026-09-15 13:51:46 +0700

    perbaikan halaman bug login

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -34,7 +34,7 @@
             <span>{{ $comments }}</span>
         </button>
         <button type="button" class="hover:text-[#6C5CE7] transition cursor-pointer ml-auto">
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+           <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-forward"><path d="m15 17 5-5-5-5"/><path d="M4 18v-2a4 4 0 0 1 4-4h12"/></svg>
         </button>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PostController; // <-- Jangan lupa import PostController
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\StatisticController;
